dfd6506132e1503d0c5c68d2ddec7537c7940c1c
FutureProgrammerLi <52388490+FutureProgrammerLi@users.noreply.github.com>
2025-03-24 19:41:30 +0800

    test token

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,10 @@
 /// <reference path="../.astro/types.d.ts" />
+/// <reference types="astro/client" />
+
+interface ImportMetaEnv {
+    readonly BLOB_READ_WRITE_TOKEN: string;
+}
+
+interface ImportMeta {
+    readonly env: ImportMetaEnv;
+}
