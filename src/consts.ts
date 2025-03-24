dc956be78b8bcb00e69820f98939c535016511bd
FutureProgrammerLi <52388490+FutureProgrammerLi@users.noreply.github.com>
2025-03-24 19:11:10 +0800

    first commit

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Lis Blogs';
+export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const MY_GITHUB = 'https://github.com/FutureProgrammerLi';
+export const MY_ANOTHER_BLOG = 'https://lisnotes.netlify.app';
